f3a60549903f427d8fd061a76b3ea237115dab23
Gravita M. Putri <putri.gravita@gmail.com>
2021-12-10 09:14:20 +0700

    first commit

---
+++ io.go
@@ -0,0 +1,31 @@
+package dsp
+
+import (
+	"encoding/csv"
+	"os"
+	"strconv"
+)
+
+func ReadSignalFile(path string, sampleRate float64) (*Signal, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	signal := Signal{
+		SampleRate: sampleRate,
+		Signal:     make([]float64, 0),
+	}
+
+	csvReader := csv.NewReader(file)
+	csvData, _ := csvReader.ReadAll()
+
+	//read without header
+	for i := 1; i < len(csvData); i++ {
+		v, _ := strconv.ParseFloat(csvData[i][6], 64)
+		signal.Signal = append(signal.Signal, v)
+	}
+
+	return &signal, err
+}
